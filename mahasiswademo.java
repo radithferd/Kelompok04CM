a7faa5c1a6a9f6361deaadcc854fd14354993fd4
Adeliarztr <adeliarizkiekaputr@gmail.com>
2025-04-16 15:00:12 +0700

    by adel

--- mahasiswademo.java
+++ mahasiswademo.java
@@ -1,3 +1,28 @@
+import java.util.Scanner;
+
 public class mahasiswademo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+        while (true) {
+            System.out.println("=== MENU SISTEM AKADEMIK");
+            System.out.println("1. Tampilkan Daftar Mahasiswa");
+            System.out.println("2. Tampilkan Daftar Matakuliah");
+            System.out.println("3. Tampilkan Data Penilaian ");
+            System.out.println("4. Urutkan Mahasiswa Bedasarkan Nilai Akhir");
+            System.out.println("5. Cari Mahasiswa Bedasarkan NIM");
+            System.out.println("0. Keluar");
+            System.out.println("Pilih Menu");
+            int pilihan = sc.nextInt();
+            sc.nextLine();
+            
+            
+            
+            
+
+            
+        }
+    }
     
 }
