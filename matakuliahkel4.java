98c3e17e31b7407a916cb2c56b5b1c7b11dec1b1
meylaniic <meylaniic@gmail.com>
2025-04-16 15:10:57 +0700

    by meylani

--- matakuliahkel4.java
+++ matakuliahkel4.java
@@ -2,4 +2,16 @@ public class matakuliahkel4 {
     String kodemk;
     String namamk;
     int sks;
+
+    matakuliahkel4 (String kodemk, String namamk, int sks) {
+        kodemk = this.kodemk;
+        namamk = this.namamk;
+        sks = this.sks;
+    }
+
+    void tampilmatkul() {
+        System.out.println("Kode Matakuliah :  " + kodemk );
+        System.out.println("Nama Matakuliah :  " + namamk);
+        System.out.println("Jumlah SKS : " + sks );
+    }
 }
